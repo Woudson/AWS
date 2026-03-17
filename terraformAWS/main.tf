ae13c7dcbf3b63fdf3e1381bf505246f2c6db74e
Woudson <91691436+Woudson@users.noreply.github.com>
2026-03-17 10:48:50 -0300

    priv

--- terraformAWS/main.tf
+++ terraformAWS/main.tf
@@ -77,7 +77,7 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   associate_public_ip_address = true
-  key_name                    = "RSA_ServerPub"
+  key_name                    = "RSA_ServerPriv"
 
   tags = {
     Name = "terraform-ec2"
