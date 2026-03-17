59d591438920d36c0fb75d0dedd0eeea9c9b3e96
Woudson <91691436+Woudson@users.noreply.github.com>
2026-03-17 10:39:35 -0300

    v2

--- terraformAWS/main.tf
+++ terraformAWS/main.tf
@@ -1,98 +1,75 @@
-provider "aws" {
-  region = "sa-east-1"
+# Configurando a VPC, Subnet, IGW, RT e SG
+
+resource "aws_vpc" "main"{
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        name = "vpc-terraform"
+    }
 }
+resource "aws_subnet" "subnet"{
 
-# 🔐 VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "sa-eats-1a"
+    map_public_ip_on_launch = true
 
-  tags = {
-    Name = "vpc-terraform"
-  }
+    tags = {
+        name = "subnet_terraform"
+    }
 }
-
-# 🌐 Subnet
-resource "aws_subnet" "subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "sa-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "subnet-terraform"
-  }
+resource "aws_internet_gateway" "igw"{
+    vpc_id = aws_vpc.main.id
 }
-
-# 🌍 Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-}
-
-# 🛣️ Route Table
 resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.main.id
+    vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+     route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
 }
-
-# 🔗 Associação
 resource "aws_route_table_association" "rta" {
   subnet_id      = aws_subnet.subnet.id
   route_table_id = aws_route_table.rt.id
 }
 
-# 🔥 Security Group
-resource "aws_security_group" "sg" {
-  vpc_id = aws_vpc.main.id
+resource "aws_security_group" "sg"{
+    vpc_id = aws_vpc.main.id
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
-# 🖥️ EC2
+
+# Configurando a maquina virtual EC2
+
 resource "aws_instance" "web" {
-  ami           = "ami-06a73f9d93a3879b5"
-  instance_type = "t2.micro"
+    ami = "ami-06a73f9d93a3879b5"
+    instance_type = "t2.micro"
 
-  subnet_id              = aws_subnet.subnet.id
-  vpc_security_group_ids = [aws_security_group.sg.id]
-
-  associate_public_ip_address = true
-  key_name                    = "RSA_ServerPub"
-  depends_on = [
-  aws_subnet.subnet,
-  aws_internet_gateway.igw
-]
-
-  tags = {
-    Name = "terraform-ec2"
-  }
-}
-
-# 📡 Output
-output "public_ip" {
-  value = aws_instance.web.public_ip
+    subnet_id = aws_subnet.subnet.id
+    vpc_security_group.ids = [aws_security_group.sg.id]
+    associate_public_ip_address = true
+    key_name = "RSA_ServerPub"
+    tags = {
+        Name = "terraform-ec2"
+    }
 }
