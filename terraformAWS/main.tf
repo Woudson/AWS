5b32861efbe9cde57dd08d9bdfd04e85b03885bd
Woudson <91691436+Woudson@users.noreply.github.com>
2026-03-17 09:38:32 -0300

    Projeto pequeno terraform

--- terraformAWS/main.tf
+++ terraformAWS/main.tf
@@ -1,4 +1,68 @@
-resource "aws_instance" "web" {
+# Configurando a VPC, Subnet, IGW, RT e SG
+
+resource "aws_vpc" "main"{
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        name = "vpc-terraform"
+    }
+}
+resource "aws_subnet" "subnet"{
+
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "sa-eats-1"
+
+    tags = {
+        name = "subnet_terraform"
+    }
+}
+resource "aws_internet_gateway" "igw"{
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+}
+resource "aws_route_table_associatio" "rta" {
+    subnet_id = aws_subnet.subnet.id
+    route_table_id = aws_route_table.rt.id
+}
+resource "aws_security_group" "sg"{
+    vpc_id = aws_vpc.main.id
+
+    ingress {
+        description = "SSH"
+        from_port = "22"
+        to_port = "22"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "HTTP"
+        from_port = "80"
+        to_port = "80"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = "0"
+        to_port = "0"
+        protocol = "-1"
+        cidr_blocks = ["o.0.0.0/0"]
+    }
+}
+
+# Configurando a maquina virtual EC2
+
+resource "aws_instance""web" {
     ami = "ami-06a73f9d93a3879b5"
-    instance_type = var.instance_type
+    instance_type = "t2.micro"
+    subnet_id = aws_sunet.sebnet.id
+    vpc_security_group.ids = [aws_security_group.sg.id]
+    associate_public_ip_adress = true
+    key_name = "RSA_ServerPub"
+    tags = {
+        Name = "terraform-ec2"
+    }
 }
--- terraformAWS/output.tf
+++ terraformAWS/output.tf
@@ -2,3 +2,7 @@ output "private_instance_ip" {
   value       = aws_instance.web.private_ip
   description = "Instancia Privada"
 }
+output "public_ip"{
+  value = aws_instance.web.public.ip
+  description = "Instancia Publica"
+}
