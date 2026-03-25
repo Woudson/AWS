699cd2b5e240a3da8076c29cdde475052c6e213d
woudson.passos <woudsonhenrique@gmail.com>
2026-03-25 16:39:48 -0300

    delete

--- terraformAWS/output.tf
+++
@@ -1,8 +0,0 @@
-output "private_instance_ip" {
-  value       = aws_instance.web.private_ip
-  description = "Instancia Privada"
-}
-output "public_ip"{
-  value = aws_instance.web.public.ip
-  description = "Instancia Publica"
-}
