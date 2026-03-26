d45aee22e68d3ff9d813490f84086c7d470229c6
woudson.passos <woudsonhenrique@gmail.com>
2026-03-26 09:41:21 -0300

    update

---
+++ aws-high-availability-project/nginx/user-data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yum update -y
+yum install -y nginx
+
+systemctl start nginx
+systemctl enable nginx
+
+echo "<h1> Servidor AWS - $(hostname) </h1>" > /usr/share/nginx/html/index.html
